3b0beb1eb9940b41895011a2725338df43e0a88e
Vinh Dang Duc <vinhdang@VinhDD.local>
2017-07-20 22:04:34 +0700

    Create project of exercise 1

---
+++ Exercise 1/Training iOS HoaLT Exercise 1/Training iOS HoaLT Exercise 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Training iOS HoaLT Exercise 1
+//
+//  Created by Vinh Dang Duc on 7/20/17.
+//  Copyright © 2017 Vinh Dang Duc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Exercise 1/Training iOS HoaLT Exercise 1/Training iOS HoaLT Exercise 1Tests/Training_iOS_HoaLT_Exercise_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Training_iOS_HoaLT_Exercise_1Tests.swift
+//  Training iOS HoaLT Exercise 1Tests
+//
+//  Created by Vinh Dang Duc on 7/20/17.
+//  Copyright © 2017 Vinh Dang Duc. All rights reserved.
+//
+
+import XCTest
+@testable import Training_iOS_HoaLT_Exercise_1
+
+class Training_iOS_HoaLT_Exercise_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
